4a41b7ff9486b53cf698d8b9ecba3249b7ef7525
brice-morin <brice.morin1@gmail.com>
2011-10-09 13:13:44 +0200

    simulator now returns gray value 0..255

--- org.thingml.simulators/src/main/scala/org/thingml/simulators/sim2d/Param2D.scala
+++ org.thingml.simulators/src/main/scala/org/thingml/simulators/sim2d/Param2D.scala
@@ -20,6 +20,7 @@
 package org.thingml.simulators.sim2d
 
 import java.awt.{Color, Image, Graphics, Dimension}
+import java.awt.image.{BufferedImage}
 import java.io.File
 
 import javax.imageio.ImageIO
@@ -33,16 +34,26 @@ object Param2D {
   val precision = 2//we look +/- precision pixels around a given pixel to determine its value
 }
 
-class Param2D(imageURI : String = "src/main/resources/sim2d/default.png") extends JFrame {
+class Param2D(property : String = "default", imageURI : String = "src/main/resources/sim2d/default.png") extends JFrame {
  
   var points = List[Pair[Int, Int]]()
   
-  val img = ImageIO.read(new File(imageURI))
-  val panel = new JLabel(new ImageIcon(img))//new ImageLabel(/*new ImageIcon(img)*/)
+  //val img = ImageIO.read(new File(imageURI))
+  
+  val grayScalaimage = new BufferedImage(Param2D.maxX + 1, Param2D.maxY + 1, BufferedImage.TYPE_BYTE_GRAY)
+  val g = grayScalaimage.getGraphics()
+  g.drawImage(ImageIO.read(new File(imageURI)), 0, 0, null)
+  g.dispose()
+  
+  
+  
+  val panel = new JLabel(new ImageIcon(grayScalaimage))//new ImageLabel(/*new ImageIcon(img)*/)
   add(panel)
   setPreferredSize(new Dimension(640, 640))
+  setTitle(property + " [" + imageURI + "]")
   pack
   setVisible(true)
+  
   
 
   def save {
@@ -54,45 +65,55 @@ class Param2D(imageURI : String = "src/main/resources/sim2d/default.png") extend
     var div = 0
     for(i <- Math.max(0, x-Param2D.precision) to Math.min(Param2D.maxX, x+Param2D.precision)){
       for(j <- Math.max(0, y-Param2D.precision) to Math.min(Param2D.maxY, y+Param2D.precision)){
-        sum = sum + img.getRGB(i, j)
+        sum = sum + grayScalaimage.getRGB(i, j)
         div = div + 1
       }
     }
+    
+    val res = sum/div
+    
+    //val alpha = (res >> 24) & 0xff;
+    val red = (res >> 16) & 0xff;
+    val green = (res >> 8) & 0xff;
+    val blue = (res) & 0xff;
+    
+    val grayVal = (0.3*red + 0.59*green + 0.11*blue).toInt
+    
+    println(List(red, green, blue).mkString(", "))
+    
     points = points :+ (x,y)
     actor{paint}
-    return sum/div
+    return grayVal
   }
   
   def paint() {
     //panel.setPosition(x, y)
     panel.getGraphics.drawPolyline(points.collect{case p => p._1}.toArray, points.collect{case p => p._2}.toArray, points.length)
-    /*panel.getGraphics.drawRect(x, y, Param2D.precision, Param2D.precision)
-     points.lastOption match {
-     case Some(p) => panel.getGraphics.drawLine(p._1, p._2, x, y)
-     case None =>
-     }
-     points = points :+ ((x,y))*/
+    points.lastOption match {
+      case Some(p) => panel.getGraphics.drawRect(p._1 - Param2D.precision, p._2 - Param2D.precision, 2*Param2D.precision, 2*Param2D.precision)
+      case None =>
+    }
   }
   
 }
 
 /*class ImageLabel(/*img : Icon*/) extends JLabel(/*img : Icon*/) {
-  //var xPosition, yPosition : Int = _
-  var points = List[Pair[Int, Int]]()
+ //var xPosition, yPosition : Int = _
+ var points = List[Pair[Int, Int]]()
   
-  def setPosition(x : Int, y : Int) {
-    points = points :+ ((x, y))
-    //this.xPosition = x
-    //this.yPosition = y
-    //println("before repaint")
-    repaint(0, 0, 0, Param2D.maxX, Param2D.maxY)
-  }
+ def setPosition(x : Int, y : Int) {
+ points = points :+ ((x, y))
+ //this.xPosition = x
+ //this.yPosition = y
+ //println("before repaint")
+ repaint(0, 0, 0, Param2D.maxX, Param2D.maxY)
+ }
   
-  override def paintComponent(g : Graphics) {
-    println("paintComponent")
-    super.paintComponent(g)
-    //getGraphics.setColor(Color.RED)
-    //getGraphics.drawRect(xPosition, yPosition, Param2D.precision, Param2D.precision)
-    getGraphics.drawPolyline(points.collect{case p => p._1}.toArray, points.collect{case p => p._2}.toArray, points.length)
-  }
-}*/
+ override def paintComponent(g : Graphics) {
+ println("paintComponent")
+ super.paintComponent(g)
+ //getGraphics.setColor(Color.RED)
+ //getGraphics.drawRect(xPosition, yPosition, Param2D.precision, Param2D.precision)
+ getGraphics.drawPolyline(points.collect{case p => p._1}.toArray, points.collect{case p => p._2}.toArray, points.length)
+ }
+ }*/
