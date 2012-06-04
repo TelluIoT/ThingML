719b4047c1e09ff090169aebfded2030738bc8b2
brice-morin <brice.morin1@gmail.com>
2012-06-04 16:53:30 +0200

    update serial communication on Java/Scala side (on going work)

---
+++ org.thingml.utils/src/main/java/org/thingml/comm/rxtx/NativeLibUtil.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (C) 2011 SINTEF <franck.fleurey@sintef.no>
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.thingml.comm.rxtx;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Francois Fouquet
+ */
+public class NativeLibUtil {
+
+    public static void copyFile(InputStream in, String to) {
+        OutputStream out = null;
+        try {
+            out = new FileOutputStream(to);
+            while (true) {
+                int data = in.read();
+                if (data == -1) {
+                    break;
+                }
+                out.write(data);
+            }
+            in.close();
+            out.close();
+        } catch (IOException ex) {
+            Logger.getLogger(NativeLibUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(NativeLibUtil.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(NativeLibUtil.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+}
