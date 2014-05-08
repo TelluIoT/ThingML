cd96f0d7b5f67af0bbd17f34bc2a475d7b05e75a
brice-morin <brice.morin1@gmail.com>
2014-05-08 16:52:36 +0200

    updated datatypes with scala types

---
+++ org.thingml.utils/src/main/java/org/thingml/comm/rxtx/SerializationHelper.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2014 SINTEF <franck.fleurey@sintef.no>
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thingml.comm.rxtx;
+
+/**
+ * Created by bmori on 08.05.2014.
+ */
+public class SerializationHelper {
+
+    public static short toShort(byte bytes[]) {
+        return 0;
+    }
+
+    public static int toInt(byte bytes[]) {
+        return 0;
+    }
+
+    public static byte[] toBytes(short s) {
+        return null;
+    }
+
+    public static byte[] toBytes(int i) {
+        return null;
+    }
+}
