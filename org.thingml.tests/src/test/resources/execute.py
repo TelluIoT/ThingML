584f000ee2d8c7e81b0c93fc70b6c0b4087f5b50
brice-morin <brice.morin1@gmail.com>
2014-08-07 15:05:08 +0200

    remove a test

--- org.thingml.tests/src/test/resources/execute.py
+++ org.thingml.tests/src/test/resources/execute.py
@@ -130,7 +130,10 @@ def generic_findCPUandMEM(type):
 			print("Impossible to run ps command")
 	if type == "Scala" or type == "Java":
 		currentDirectory = os.getcwd()
-		os.chdir("../../../../org.thingml.tests/target/results/Scala")
+		if type == "Scala":
+			os.chdir("../../../../org.thingml.tests/target/results/Scala")
+		else:
+			os.chdir("../../../../org.thingml.tests/target/results/Java")
 		mypath = "."
 		onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 		snapshotName="test"
