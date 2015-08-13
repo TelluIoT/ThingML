e8d560b6b8f589d0a73d18305fe0973ff581d8eb
Ludovic Mouline <mouline.ludovic@etu.unice.fr>
2015-08-13 15:14:41 +0200

    add a tests for length window

--- org.thingml.tests/configuration.py
+++ org.thingml.tests/configuration.py
@@ -21,13 +21,13 @@ deleteTemporaryFiles = True
 #Chooses which compilers should be used
 testLanguages=[]
 # testLanguages.append("Linux")
-# testLanguages.append("Javascript")
-testLanguages.append("Java")
-#testLanguages.append("Arduino")
+testLanguages.append("Javascript")
+# testLanguages.append("Java")
+# testLanguages.append("Arduino")
 
 #Functional tests options
 #If useBlacklist is True, runs all tests not present in blacklist
 #If useBlacklist is False, runs all tests present in whitelist
-useBlacklist=True
+useBlacklist=False
 blacklist=("tester","datatypes","thingml")
-whitelist=("testSimpleFilter","testMergeFilter", "testJoinFilter")
+whitelist=("testLengthSimpleSource")
