7067c1b1fe2ea2cc7c2aa1b51100bbe8b5200957
brice-morin <brice.morin1@gmail.com>
2015-08-19 16:03:03 +0200

    fix #93

--- org.thingml.tests/configuration.py
+++ org.thingml.tests/configuration.py
@@ -20,9 +20,9 @@ deleteTemporaryFiles = True
 
 #Chooses which compilers should be used
 testLanguages=[]
-testLanguages.append("Linux")
+# testLanguages.append("Linux")
 # testLanguages.append("Javascript")
-# testLanguages.append("Java")
+testLanguages.append("Java")
 # testLanguages.append("Arduino")
 
 #Functional tests options
@@ -30,5 +30,4 @@ testLanguages.append("Linux")
 #If useBlacklist is False, runs all tests present in whitelist
 useBlacklist=True
 blacklist=("tester","datatypes","thingml")
-#whitelist=("testArrays", "testNaming", "testInit", "testArrays3")
-whitelist=("testInit")
+whitelist=("testMultiServerPingTest")
