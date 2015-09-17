1966f76a201188fe4da3481a64482db81c3dbf63
sintef <nicolas.harrand@gmail.com>
2015-09-17 15:15:36 +0200

    [bug fix] Fix a bug concerning external connector with C compiler

--- compilers/c/src/main/java/org/thingml/compilers/c/CCfgMainGenerator.java
+++ compilers/c/src/main/java/org/thingml/compilers/c/CCfgMainGenerator.java
@@ -135,7 +135,8 @@ public class CCfgMainGenerator extends CfgMainGenerator {
                     StringBuilder eco_instance = new StringBuilder();
                     eco_instance.append("//Connector");
                     Port p = eco.getPort();
-                    if(!p.getSends().isEmpty()) {
+                    if(!p.getReceives().isEmpty()) {
+                    //if(!p.getSends().isEmpty()) {
                         eco_instance.append("// Pointer to receiver list\n");
                         eco_instance.append("struct Msg_Handler ** ");
                         eco_instance.append(p.getName());
@@ -147,7 +148,8 @@ public class CCfgMainGenerator extends CfgMainGenerator {
                     }
 
 
-                    if(!p.getReceives().isEmpty()) {
+                    if(!p.getSends().isEmpty()) {
+                    //if(!p.getReceives().isEmpty()) {
                         eco_instance.append("// Handler Array\n");
                         eco_instance.append("struct Msg_Handler * ");
                         eco_instance.append(p.getName());
@@ -248,7 +250,7 @@ public class CCfgMainGenerator extends CfgMainGenerator {
                     StringBuilder eco_instance = new StringBuilder();
                     eco_instance.append("//Connector");
                     Port p = eco.getPort();
-                    if(!p.getSends().isEmpty()) {
+                    if(!p.getReceives().isEmpty()) {
                         eco_instance.append("// Pointer to receiver list\n");
                         eco_instance.append("struct Msg_Handler ** ");
                         eco_instance.append(p.getName());
@@ -285,7 +287,7 @@ public class CCfgMainGenerator extends CfgMainGenerator {
                     }
                     
                     
-                    if(!p.getReceives().isEmpty()) {
+                    if(!p.getSends().isEmpty()) {
                         eco_instance.append("// Handler Array\n");
                         eco_instance.append("struct Msg_Handler * ");
                         eco_instance.append(p.getName());
@@ -337,7 +339,8 @@ public class CCfgMainGenerator extends CfgMainGenerator {
                     StringBuilder eco_instance = new StringBuilder();
                     eco_instance.append("//Connector");
                     Port p = eco.getPort();
-                    if(!p.getSends().isEmpty()) {
+                    if(!p.getReceives().isEmpty()) {
+                    //if(!p.getSends().isEmpty()) {
                         eco_instance.append("// Pointer to receiver list\n");
                         eco_instance.append("struct Msg_Handler ** ");
                         eco_instance.append(p.getName());
@@ -347,7 +350,8 @@ public class CCfgMainGenerator extends CfgMainGenerator {
                         eco_instance.append(p.getName());
                         eco_instance.append("_receiver_list_tail;\n");
                     }
-                    if(!p.getReceives().isEmpty()) {
+                    //if(!p.getReceives().isEmpty()) {
+                    if(!p.getSends().isEmpty()) {
                         eco_instance.append("// Handler Array\n");
                         eco_instance.append("struct Msg_Handler * ");
                         eco_instance.append(p.getName());
@@ -539,16 +543,17 @@ public class CCfgMainGenerator extends CfgMainGenerator {
                     htemplate = htemplate.replace("/*PATH_TO_C*/", eco.getInst().getInstance().getName() 
                             + "_" + eco.getPort().getName() + "_" + eco.getProtocol() + ".c");
                     
-                    if(!eco.getPort().getReceives().isEmpty()) {
-                        List<String> topicList = eco.annotation("mqtt_topic");
-                        if(topicList.isEmpty()) {
-                            topicList.add("ThingML");
-                        }
-                        if(topicList.size() > 1) {
-                            ctemplate = ctemplate.replace("/*TOPIC_VAR*/", "static char **" 
-                                    + portName + "_topics[" + topicList.size() + "];\n"
-                                    + "static int " + portName + "_topic_count = " + topicList.size() + ";"
-                            );
+                    //if(!eco.getPort().getReceives().isEmpty()) {
+                    List<String> topicList = eco.annotation("mqtt_topic");
+                    if(topicList.isEmpty()) {
+                        topicList.add("ThingML");
+                    }
+                    if(topicList.size() > 1) {
+                        ctemplate = ctemplate.replace("/*TOPIC_VAR*/", "static char **" 
+                                + portName + "_topics[" + topicList.size() + "];\n"
+                                + "static int " + portName + "_topic_count = " + topicList.size() + ";"
+                        );
+                        if(!eco.getPort().getReceives().isEmpty()) {
                             ctemplate = ctemplate.replace("/*SUBSCRUBE_MULTI_OR_MONO*/", "for(i=0; i<"
                                     + portName + "_topic_count; i++){\n"
                                     + "mosquitto_subscribe("
@@ -556,81 +561,94 @@ public class CCfgMainGenerator extends CfgMainGenerator {
                                     + portName + "_topics[i], "
                                     + portName + "_topic_qos);\n"
                                     + "}\n");
-                            StringBuilder topicsInit = new StringBuilder();
-                            int j = 0;
-                            for(String topic : topicList) {
-                                topicsInit.append(portName + "_topics[" + j + "] = \"" + topic + "\";\n");
-                                j++;
+                        }
+                        StringBuilder topicsInit = new StringBuilder();
+                        int j = 0;
+                        for(String topic : topicList) {
+                            topicsInit.append(portName + "_topics[" + j + "] = \"" + topic + "\";\n");
+                            j++;
+                        }
+                        ctemplate = ctemplate.replace("/*MULTI_TOPIC_INIT*/", topicsInit);
+
+                        String publishSelection = null;
+                        boolean publishSelect = false;
+                        if(eco.hasAnnotation("mqtt_multi_topic_publish_selection")) {
+                            publishSelection = eco.annotation("mqtt_multi_topic_publish_selection").iterator().next();
+                        }
+                        if(publishSelection != null) {
+                            if(publishSelection.compareTo("true") == 0) {
+                            publishSelect = true;
                             }
-                            ctemplate = ctemplate.replace("/*MULTI_TOPIC_INIT*/", topicsInit);
+                        }
+                        
+                        if(publishSelect) {
+                            ctemplate = ctemplate.replace("/*PUBLISH_MULTI_OR_MONO_DECLARATION*/", ", uint16_t topicID");
+                            htemplate = htemplate.replace("/*PUBLISH_MULTI_OR_MONO_DECLARATION*/", ", uint16_t topicID");
                             
-                            String publishSelection = null;
-                            boolean publishSelect = false;
-                            if(eco.hasAnnotation("mqtt_multi_topic_publish_selection")) {
-                                publishSelection = eco.annotation("mqtt_multi_topic_publish_selection").iterator().next();
-                            }
-                            if(publishSelection != null) {
-                                if(publishSelection.compareTo("true") == 0) {
-                                publishSelect = true;
-                                }
-                            }
-                            
-                            if(publishSelect) {
-                                ctemplate = ctemplate.replace("/*PUBLISH_MULTI_OR_MONO_DECLARATION*/", ", uint16_t topicID");
-                                htemplate = htemplate.replace("/*PUBLISH_MULTI_OR_MONO_DECLARATION*/", ", uint16_t topicID");
-                                
-                                ctemplate = ctemplate.replace("/*PUBLISH_MULTI_OR_MONO_CORE*/", ""
-                                        + "if(topicID == 65535) {"
-                                        + "int j;\n"
-                                        + "for(j = 0; j < "
-                                        + portName + "_topic_count; j++) {\n"
-                                        + "/*TRACE_LEVEL_3*/printf(\"["
-                                        + portName + "] publish:\\\"%s\\\" on topic: %s \\n\", p, "
-                                        + portName + "_topics[j]);\n"
-                                        + "mosquitto_publish(" + portName + "_mosq, "
-                                        + "&" + portName + "_mid_sent, "
-                                        + portName + "_topics[j], "
-                                        + "(length * 3 + 1), "
-                                        + "p, "
-                                        + portName + "_qos, "
-                                        + portName + "_retain);\n"
-                                        + "}\n} else {\n"
-                                        + "/*TRACE_LEVEL_3*/printf(\"["
-                                        + portName + "] publish:\\\"%s\\\" on topic: %s \\n\", p, "
-                                        + portName + "_topics[topicID]);\n"
-                                        + "mosquitto_publish(" + portName + "_mosq, "
-                                        + "&" + portName + "_mid_sent, "
-                                        + portName + "_topics[topicID], "
-                                        + "(length * 3 + 1), "
-                                        + "p, "
-                                        + portName + "_qos, "
-                                        + portName + "_retain);\n"
-                                        + "}\n");
-                            } else {
-                                ctemplate = ctemplate.replace("/*PUBLISH_MULTI_OR_MONO_CORE*/", ""
-                                        + "int j;\n"
-                                        + "for(j = 0; j < "
-                                        + portName + "_topic_count; j++) {\n"
-                                        + "/*TRACE_LEVEL_3*/printf(\"["
-                                        + portName + "] publish:\\\"%s\\\" on topic: %s \\n\", p, "
-                                        + portName + "_topics[j]);\n"
-                                        + "mosquitto_publish(" + portName + "_mosq, "
-                                        + "&" + portName + "_mid_sent, "
-                                        + portName + "_topics[j], "
-                                        + "(length * 3 + 1), "
-                                        + "p, "
-                                        + portName + "_qos, "
-                                        + portName + "_retain);\n"
-                                        + "}\n");
+                            if(!eco.getPort().getSends().isEmpty()) {
+                            ctemplate = ctemplate.replace("/*PUBLISH_MULTI_OR_MONO_CORE*/", ""
+                                    + "if(topicID == 65535) {"
+                                    + "int j;\n"
+                                    + "for(j = 0; j < "
+                                    + portName + "_topic_count; j++) {\n"
+                                    + "/*TRACE_LEVEL_3*/printf(\"["
+                                    + portName + "] publish:\\\"%s\\\" on topic: %s \\n\", p, "
+                                    + portName + "_topics[j]);\n"
+                                    + "mosquitto_publish(" + portName + "_mosq, "
+                                    + "&" + portName + "_mid_sent, "
+                                    + portName + "_topics[j], "
+                                    + "(length * 3 + 1), "
+                                    + "p, "
+                                    + portName + "_qos, "
+                                    + portName + "_retain);\n"
+                                    + "}\n} else {\n"
+                                    + "/*TRACE_LEVEL_3*/printf(\"["
+                                    + portName + "] publish:\\\"%s\\\" on topic: %s \\n\", p, "
+                                    + portName + "_topics[topicID]);\n"
+                                    + "mosquitto_publish(" + portName + "_mosq, "
+                                    + "&" + portName + "_mid_sent, "
+                                    + portName + "_topics[topicID], "
+                                    + "(length * 3 + 1), "
+                                    + "p, "
+                                    + portName + "_qos, "
+                                    + portName + "_retain);\n"
+                                    + "}\n");
                             }
                         } else {
-                            ctemplate = ctemplate.replace("/*TOPIC_VAR*/", "static char *" 
-                                    + portName + "_topic = \"" + topicList.get(0) + "\";");
+                            ctemplate = ctemplate.replace("/*PUBLISH_MULTI_OR_MONO_DECLARATION*/", "");
+                            htemplate = htemplate.replace("/*PUBLISH_MULTI_OR_MONO_DECLARATION*/", "");
+                            if(!eco.getPort().getSends().isEmpty()) {
+                            ctemplate = ctemplate.replace("/*PUBLISH_MULTI_OR_MONO_CORE*/", ""
+                                    + "int j;\n"
+                                    + "for(j = 0; j < "
+                                    + portName + "_topic_count; j++) {\n"
+                                    + "/*TRACE_LEVEL_3*/printf(\"["
+                                    + portName + "] publish:\\\"%s\\\" on topic: %s \\n\", p, "
+                                    + portName + "_topics[j]);\n"
+                                    + "mosquitto_publish(" + portName + "_mosq, "
+                                    + "&" + portName + "_mid_sent, "
+                                    + portName + "_topics[j], "
+                                    + "(length * 3 + 1), "
+                                    + "p, "
+                                    + portName + "_qos, "
+                                    + portName + "_retain);\n"
+                                    + "}\n");
+                            }
+                        }
+                    } else {
+                        ctemplate = ctemplate.replace("/*PUBLISH_MULTI_OR_MONO_DECLARATION*/", "");
+                        htemplate = htemplate.replace("/*PUBLISH_MULTI_OR_MONO_DECLARATION*/", "");
+                        ctemplate = ctemplate.replace("/*TOPIC_VAR*/", "static char *" 
+                                + portName + "_topic = \"" + topicList.get(0) + "\";");
+                        if(!eco.getPort().getReceives().isEmpty()) {
                             ctemplate = ctemplate.replace("/*SUBSCRUBE_MULTI_OR_MONO*/", "mosquitto_subscribe("
                                     + portName + "_mosq, NULL, "
                                     + portName + "_topic, "
                                     + portName + "_topic_qos);");
-                            
+                        }
+                        
+                        if(!eco.getPort().getSends().isEmpty()) {
+
                             ctemplate = ctemplate.replace("/*PUBLISH_MULTI_OR_MONO_CORE*/", ""
                                     + "/*TRACE_LEVEL_3*/printf(\"[" + portName + "] publish:\\\"%s\\\"\\n\", p);\n"
                                     + "mosquitto_publish(" + portName + "_mosq, "
@@ -642,6 +660,7 @@ public class CCfgMainGenerator extends CfgMainGenerator {
                                     + portName + "_retain);\n");
                         }
                     }
+                    
                     
                     Integer traceLevel;
                     if(eco.hasAnnotation("trace_level")) {
@@ -1854,7 +1873,7 @@ public class CCfgMainGenerator extends CfgMainGenerator {
             builder.append(portName + "_instance." + p.getName() + "_receiver_list_tail = &");
             builder.append(cfg.getName() + "_receivers[" + (nbConnectorSoFar - 1) + "];\n");
         } else {
-            if(!p.getSends().isEmpty()) {
+            if(!p.getReceives().isEmpty()) {
                 //Case where the port could sends messages but isn't connected
                 builder.append(portName + "_instance." + p.getName() + "_receiver_list_head = ");
                 builder.append("NULL;\n");
