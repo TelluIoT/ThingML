4937fcee8577d3219999579c917861225e66e26c
sintef <nicolas.harrand@gmail.com>
2016-01-20 10:04:29 +0100

    a couple more resolver

---
+++ org.thingml.parser/src/main/java/org/sintef/thingml/resource/thingml/analysis/ExternalConnectorProtocolReferenceResolver.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) 2014 SINTEF <franck.fleurey@sintef.no>
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sintef.thingml.resource.thingml.analysis;
+
+public class ExternalConnectorProtocolReferenceResolver implements org.sintef.thingml.resource.thingml.IThingmlReferenceResolver<org.sintef.thingml.ExternalConnector, org.sintef.thingml.Protocol> {
+	
+	private org.sintef.thingml.resource.thingml.analysis.ThingmlDefaultResolverDelegate<org.sintef.thingml.ExternalConnector, org.sintef.thingml.Protocol> delegate = new org.sintef.thingml.resource.thingml.analysis.ThingmlDefaultResolverDelegate<org.sintef.thingml.ExternalConnector, org.sintef.thingml.Protocol>();
+	
+	public void resolve(String identifier, org.sintef.thingml.ExternalConnector container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, final org.sintef.thingml.resource.thingml.IThingmlReferenceResolveResult<org.sintef.thingml.Protocol> result) {
+		delegate.resolve(identifier, container, reference, position, resolveFuzzy, result);
+	}
+	
+	public String deResolve(org.sintef.thingml.Protocol element, org.sintef.thingml.ExternalConnector container, org.eclipse.emf.ecore.EReference reference) {
+		return delegate.deResolve(element, container, reference);
+	}
+	
+	public void setOptions(java.util.Map<?,?> options) {
+		// save options in a field or leave method empty if this resolver does not depend
+		// on any option
+	}
+	
+}
---
+++ org.thingml.parser/src/main/java/org/sintef/thingml/resource/thingml/analysis/ThingmlREAL_LITERALTokenResolver.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) 2014 SINTEF <franck.fleurey@sintef.no>
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sintef.thingml.resource.thingml.analysis;
+
+public class ThingmlREAL_LITERALTokenResolver implements org.sintef.thingml.resource.thingml.IThingmlTokenResolver {
+	
+	private org.sintef.thingml.resource.thingml.analysis.ThingmlDefaultTokenResolver defaultTokenResolver = new org.sintef.thingml.resource.thingml.analysis.ThingmlDefaultTokenResolver(true);
+	
+	public String deResolve(Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
+		// By default token de-resolving is delegated to the DefaultTokenResolver.
+		String result = defaultTokenResolver.deResolve(value, feature, container, null, null, null);
+		return result;
+	}
+	
+	public void resolve(String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.sintef.thingml.resource.thingml.IThingmlTokenResolveResult result) {
+		// By default token resolving is delegated to the DefaultTokenResolver.
+		defaultTokenResolver.resolve(lexem, feature, result, null, null, null);
+	}
+	
+	public void setOptions(java.util.Map<?,?> options) {
+		defaultTokenResolver.setOptions(options);
+	}
+	
+}
