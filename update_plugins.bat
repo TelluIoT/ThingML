49baef6b338cf2a51490dc25b86a52da7df280b2
brice-morin <brice.morin1@gmail.com>
2016-03-07 13:32:24 +0100

    release 0.6.0

--- update_plugins.bat
+++ update_plugins.bat
@@ -1,4 +1,4 @@
 mvn clean install
 cd org.thingml.editor.standaloneApp
 mvn -f pom_eclipse.xml clean install
-cp target/org.thingml.editor.standaloneApp-0.6.0-SNAPSHOT-jar-with-dependencies.jar ../org.thingml.eclipse.ui/lib/
+cp target/org.thingml.editor.standaloneApp-0.6.0-jar-with-dependencies.jar ../org.thingml.eclipse.ui/lib/
