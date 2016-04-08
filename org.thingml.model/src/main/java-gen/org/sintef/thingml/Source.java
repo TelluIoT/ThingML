c0e7ba0d267f103c92af125d8b214dfe84b615fa
Alexandre Rio <contact@alexrio.fr>
2016-04-08 17:25:23 +0200

    Allow annotations on CEP stream input sources

--- org.thingml.model/src/main/java-gen/org/sintef/thingml/Source.java
+++ org.thingml.model/src/main/java-gen/org/sintef/thingml/Source.java
@@ -35,7 +35,7 @@ import org.eclipse.emf.common.util.EList;
  * @model abstract="true"
  * @generated
  */
-public interface Source extends ThingMLElement, ReferencedElmt {
+public interface Source extends AnnotatedElement, ReferencedElmt {
 
 	/**
 	 * Returns the value of the '<em><b>Operators</b></em>' containment reference list.
