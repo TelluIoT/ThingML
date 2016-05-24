51000e145eb5b825238137d5b950da7059027e9d
sintef <nicolas.harrand@gmail.com>
2016-05-24 17:35:17 +0200

    bugfix uml compiler for protocol

--- compilers/uml/src/main/java/org/thingml/compilers/uml/PlantUMLCfgMainGenerator.java
+++ compilers/uml/src/main/java/org/thingml/compilers/uml/PlantUMLCfgMainGenerator.java
@@ -47,7 +47,7 @@ public class PlantUMLCfgMainGenerator extends CfgMainGenerator {
             builder.append("[" + c.getCli().getInstance().getName() + "] -> " + c.getProvided().getName() + "_" + c.getSrv().getInstance().getName() + " : " + c.getRequired().getName() + "\n");
         }
         for (ExternalConnector eco : ConfigurationHelper.getExternalConnectors(cfg)) {
-            builder.append("[" + cfg.getName() + "_" + eco.getInst().getInstance().getName() + "] ..> " + eco.getProtocol() + "\n");
+            builder.append("[" + cfg.getName() + "_" + eco.getInst().getInstance().getName() + "] ..> " + eco.getProtocol().getName() + "\n");
         }
         builder.append("}\n");
         builder.append("@enduml");
