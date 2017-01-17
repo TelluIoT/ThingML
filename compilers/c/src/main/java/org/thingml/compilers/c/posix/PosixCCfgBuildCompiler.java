TelluIoT/ThingML

commit be50520d09dee536203f525ebf1af48a56d99b4a

Fixed some bugs in the PosixMQTT plugin with new library version

compilers/c/src/main/java/org/thingml/compilers/c/posix/PosixCCfgBuildCompiler.java
@@ -26,6 +26,7 @@ import org.thingml.compilers.c.CCompilerContext;
 import org.thingml.compilers.configuration.CfgBuildCompiler;
 
 import java.io.File;
+import java.util.Set;
 
 /**
  * Created by ffl on 17.06.15.
@@ -141,6 +142,20 @@ public class PosixCCfgBuildCompiler extends CfgBuildCompiler {
             mtemplate = mtemplate.replace("/*STATIC*/", "-static ");
         } else {
             mtemplate = mtemplate.replace("/*STATIC*/", "");
+        }
+
+        // Add network plugin generated source and object files
+        Set<String> networkPluginFiles = ctx.getNetworkPluginFiles();
+        for (String cfile : networkPluginFiles) {
+            String ofile;
+            if (cfile.endsWith(".c")) {
+                ofile = cfile.substring(0, cfile.length()-2)+".o";
+            } else {
+                ofile = cfile+".o";
+                cfile = cfile+".c";
+            }
+            srcs.append(cfile+" ");
+            objs.append(ofile+" ")
         }
 
         mtemplate = mtemplate.replace("/*SOURCES*/", srcs.toString().trim());

compilers/official-network-plugins/src/main/java/org/thingml/networkplugins/c/posix/PosixMQTTPlugin.java
@@ -176,8 +176,6 @@ public class PosixMQTTPlugin extends NetworkPlugin {
                 StringBuilder eco_instance = new StringBuilder();
                 eco_instance.append("//Connector");
 
-                htemplate = htemplate.replace("/*PATH_TO_C*/", protocol.getName() + ".c");
-
                 //if(!eco.getPort().getReceives().isEmpty()) {
                 List<String> topicList = AnnotatedElementHelper.annotation(protocol, "mqtt_topic");
                 if (topicList.isEmpty()) {
@@ -367,12 +365,14 @@ public class PosixMQTTPlugin extends NetworkPlugin {
                 StringBuilder h = new StringBuilder();
                 generateMessageForwarders(b, h, cfg, protocol);
 
-                ctemplate += "\n" + b;
-                htemplate += "\n" + h;
+                ctemplate = ctemplate.replace("/*FORWARDERS*/", b);
+                htemplate = htemplate.replace("/*FORWARDERS*/", h);
 
                 ctx.getBuilder(protocol.getName() + ".c").append(ctemplate);
                 ctx.getBuilder(protocol.getName() + ".h").append(htemplate);
+
                 ctx.addToIncludes("#include \"" + protocol.getName() + ".h\"");
+                ctx.addNetworkPluginFile(protocol.getName() + ".c");
             }
         }
 

compilers/official-network-plugins/src/main/resources/templates/PosixMQTTPlugin.c
@@ -1,5 +1,3 @@
-
-
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -32,14 +30,14 @@ void /*PORT_NAME*/_set_listener_id(uint16_t id) {
 /*PARSER_IMPLEMENTATION*/
 
 
-void /*PORT_NAME*/_publish_callback(void *obj, uint16_t mid)
+void /*PORT_NAME*/_publish_callback(struct mosquitto *mosq, void *obj, uint16_t mid)
 {
 	struct mosquitto *mosq = obj;
 
 	
 }
 
-void /*PORT_NAME*/_message_callback(void *obj, const struct mosquitto_message *message)
+void /*PORT_NAME*/_message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message)
 {
     printf("%s %s\n", message->topic, message->payload);
     int len = strlen(message->payload);
@@ -47,7 +45,7 @@ void /*PORT_NAME*/_message_callback(void *obj, const struct mosquitto_message *m
     /*PORT_NAME*/_parser(message->payload, len);
 }
 
-void /*PORT_NAME*/_connect_callback(void *obj, int result)
+void /*PORT_NAME*/_connect_callback(struct mosquitto *mosq, void *obj, int result)
 {
     struct mosquitto *mosq = obj;
 
@@ -78,7 +76,7 @@ void /*PORT_NAME*/_connect_callback(void *obj, int result)
     }
 }
 
-void /*PORT_NAME*/_subscribe_callback(void *obj, uint16_t mid, int qos_count, const uint8_t *granted_qos)
+void /*PORT_NAME*/_subscribe_callback(struct mosquitto *mosq, void *obj, uint16_t mid, int qos_count, const uint8_t *granted_qos)
 {
 	int i;
 
@@ -146,7 +144,7 @@ void /*PORT_NAME*/_setup() {
 		/*TRACE_LEVEL_1*/fprintf(stderr, "[/*PORT_NAME*/] Warning: Not using password since username not set.\n");
 	}
 	mosquitto_lib_init();
-	/*PORT_NAME*/_mosq = mosquitto_new(id, NULL);
+	/*PORT_NAME*/_mosq = mosquitto_new(id, clean_session, NULL);
 	if(!/*PORT_NAME*/_mosq){
 		/*TRACE_LEVEL_1*/fprintf(stderr, "[/*PORT_NAME*/] Error: Out of memory.\n");
 		return 1;
@@ -164,7 +162,7 @@ void /*PORT_NAME*/_setup() {
 	
 	/*TRACE_LEVEL_1*/mosquitto_subscribe_callback_set(/*PORT_NAME*/_mosq, /*PORT_NAME*/_subscribe_callback);
 
-	rc = mosquitto_connect(/*PORT_NAME*/_mosq, host, port, keepalive, clean_session);
+	rc = mosquitto_connect(/*PORT_NAME*/_mosq, host, port, keepalive);
 	if(rc){
 		/*TRACE_LEVEL_1*/if(rc == MOSQ_ERR_ERRNO){
 			/*TRACE_LEVEL_1*/strerror_r(errno, err, 1024);
@@ -181,7 +179,7 @@ void /*PORT_NAME*/_start_receiver_process() {
         int rc;	
 
 	do{
-		rc = mosquitto_loop(/*PORT_NAME*/_mosq, -1);
+		rc = mosquitto_loop(/*PORT_NAME*/_mosq, -1, 1);
 	}while(rc == MOSQ_ERR_SUCCESS);
 
         /*TRACE_LEVEL_1*/printf("[/*PORT_NAME*/] Error :%i\n", rc);
@@ -209,3 +207,4 @@ void /*PORT_NAME*/_forwardMessage(uint8_t * msg, int size/*PUBLISH_MULTI_OR_MONO
     
 }
 
+/*FORWARDERS*/

compilers/official-network-plugins/src/main/resources/templates/PosixMQTTPlugin.h
@@ -1,20 +1,16 @@
 #ifndef /*PORT_NAME*/_PosixMQTT_Client_h
 #define  /*PORT_NAME*/_PosixMQTT_Client_h
 
-
-
-#include "/*PATH_TO_C*/"/*PORT_NAME*/_instance;
-
 struct /*PORT_NAME*/_instance_type /*PORT_NAME*/_instance;
 
-
 void /*PORT_NAME*/_set_listener_id(uint16_t id);
-
 
 void /*PORT_NAME*/_setup();
 
 void /*PORT_NAME*/_start_receiver_process();
 
 void /*PORT_NAME*/_forwardMessage(uint8_t * msg, int size/*PUBLISH_MULTI_OR_MONO_DECLARATION*/);
+
+/*FORWARDERS*/
 
 #endif

compilers/official-network-plugins/src/main/resources/templates/PosixMQTTPluginX86.c
@@ -1,5 +1,3 @@
-
-
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -34,11 +32,11 @@ void /*PORT_NAME*/_set_listener_id(uint16_t id) {
 /*PARSER_IMPLEMENTATION*/
 
 
-void /*PORT_NAME*/_publish_callback(void *obj, uint16_t mid)
+void /*PORT_NAME*/_publish_callback(struct mosquitto *mosq, void *obj, uint16_t mid)
 {
 }
 
-void /*PORT_NAME*/_message_callback(void *obj, const struct mosquitto_message *message)
+void /*PORT_NAME*/_message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message)
 {
     //printf("%s %s\n", message->topic, message->payload);
     int len = strlen(message->payload);
@@ -47,7 +45,7 @@ void /*PORT_NAME*/_message_callback(void *obj, const struct mosquitto_message *m
     
 }
 
-void /*PORT_NAME*/_connect_callback(void *obj, int result)
+void /*PORT_NAME*/_connect_callback(struct mosquitto *mosq, void *obj, int result)
 {
 
     int i;
@@ -77,7 +75,7 @@ void /*PORT_NAME*/_connect_callback(void *obj, int result)
     }
 }
 
-void /*PORT_NAME*/_subscribe_callback(void *obj, uint16_t mid, int qos_count, const uint8_t *granted_qos)
+void /*PORT_NAME*/_subscribe_callback(struct mosquitto *mosq, void *obj, uint16_t mid, int qos_count, const uint8_t *granted_qos)
 {
 	int i;
 
@@ -146,7 +144,7 @@ void /*PORT_NAME*/_setup() {
 		/*TRACE_LEVEL_1*/fprintf(stderr, "[/*PORT_NAME*/] Warning: Not using password since username not set.\n");
 	}
 	//mosquitto_threaded_set(MQTT_mosq, true);
-	/*PORT_NAME*/_mosq = mosquitto_new(id, NULL);
+	/*PORT_NAME*/_mosq = mosquitto_new(id, clean_session, NULL);
 	if(!/*PORT_NAME*/_mosq){
 		/*TRACE_LEVEL_1*/fprintf(stderr, "[/*PORT_NAME*/] Error: Out of memory.\n");
 		return 1;
@@ -160,7 +158,7 @@ void /*PORT_NAME*/_setup() {
 	
 	/*TRACE_LEVEL_1*/mosquitto_subscribe_callback_set(/*PORT_NAME*/_mosq, /*PORT_NAME*/_subscribe_callback);
 
-	rc = mosquitto_connect(/*PORT_NAME*/_mosq, host, port, keepalive, clean_session);
+	rc = mosquitto_connect(/*PORT_NAME*/_mosq, host, port, keepalive);
 	if(rc){
 		/*TRACE_LEVEL_1*/if(rc == MOSQ_ERR_ERRNO){
 			/*TRACE_LEVEL_1*/strerror_r(errno, err, 1024);
@@ -177,7 +175,7 @@ void /*PORT_NAME*/_start_receiver_process() {
         int rc;	
 
 	do{
-		rc = mosquitto_loop(/*PORT_NAME*/_mosq, -1);
+		rc = mosquitto_loop(/*PORT_NAME*/_mosq, -1, 1);
 	}while(rc == MOSQ_ERR_SUCCESS);
 
         /*TRACE_LEVEL_1*/printf("[/*PORT_NAME*/] Error :%i\n", rc);
@@ -205,3 +203,4 @@ void /*PORT_NAME*/_forwardMessage(uint8_t * msg, int size/*PUBLISH_MULTI_OR_MONO
     
 }
 
+/*FORWARDERS*/

compilers/c/src/main/java/org/thingml/compilers/c/CCompilerContext.java
@@ -51,6 +51,7 @@ public abstract class CCompilerContext extends Context {
     StringBuilder initCode = new StringBuilder();
     StringBuilder includeCode = new StringBuilder();
     StringBuilder cppHeaderCode = new StringBuilder();
+    Set<String> networkPluginFiles = new HashSet<String>();
     private Set<NetworkLibraryGenerator> NetworkLibraryGenerators;
     private Map<String, Map<String, String>> mapCepMsgParamAndStream;
     
@@ -859,6 +860,8 @@ public abstract class CCompilerContext extends Context {
         return cppHeaderCode;
     }
 
+    public void addNetworkPluginFile(String cfile) { networkPluginFiles.add(cfile); }
+    public Set<String> getNetworkPluginFiles() { return networkPluginFiles; }
 
     public void generatePSPollingCode(Configuration cfg, StringBuilder builder) {}
 

commit e336fe7f7242b70f5a9a919f6ec71f8c1c17042f

Fixed some more bugs introduced in be50520d09dee536203f525ebf1af48a56d99b4a
Including a missing ;

compilers/c/src/main/java/org/thingml/compilers/c/posix/PosixCCfgBuildCompiler.java
@@ -155,7 +155,7 @@ public class PosixCCfgBuildCompiler extends CfgBuildCompiler {
                 cfile = cfile+".c";
             }
             srcs.append(cfile+" ");
-            objs.append(ofile+" ")
+            objs.append(ofile+" ");
         }
 
         mtemplate = mtemplate.replace("/*SOURCES*/", srcs.toString().trim());

compilers/official-network-plugins/src/main/java/org/thingml/networkplugins/c/posix/PosixMQTTPlugin.java
@@ -22,7 +22,9 @@
 package org.thingml.networkplugins.c.posix;
 
 import org.sintef.thingml.*;
+import org.sintef.thingml.constraints.ThingMLHelpers;
 import org.sintef.thingml.helpers.AnnotatedElementHelper;
+import org.sintef.thingml.helpers.ConfigurationHelper;
 import org.sintef.thingml.impl.ThingmlFactoryImpl;
 import org.thingml.compilers.Context;
 import org.thingml.compilers.c.CCompilerContext;
@@ -132,6 +134,17 @@ public class PosixMQTTPlugin extends NetworkPlugin {
                 String htemplate = ctx.getTemplateByID("templates/PosixMQTTPlugin.h");
 
                 String portName = protocol.getName();
+
+                // TODO Jakob, this is kind of hack-ish
+                // Section copied from CCfgMainGenerator.java - generateIncludes
+                StringBuilder includes = new StringBuilder();
+                ThingMLModel model = ThingMLHelpers.findContainingModel(cfg);
+                for (Thing t : ConfigurationHelper.allThings(cfg)) {
+                  includes.append("#include \"" + t.getName() + ".h\"\n");
+                }
+                includes.append(ctx.getIncludeCode());
+                htemplate = htemplate.replace("/*INCLUDES*/",includes);
+                // End hack-ish section
 
                 //Threaded listener --- BEGIN
                 ctx.addToInitCode("\n" + portName + "_instance.listener_id = add_instance(&" + portName + "_instance);\n");

compilers/official-network-plugins/src/main/resources/templates/PosixMQTTPlugin.c
@@ -1,9 +1,4 @@
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#include <mosquitto.h>
+#include "/*PORT_NAME*/.h"
 
 /*TOPIC_VAR*/
 static int /*PORT_NAME*/_topic_qos = 0;
@@ -14,12 +9,6 @@ static int /*PORT_NAME*/_retain = 0;
 static uint16_t /*PORT_NAME*/_mid_sent = 0;
 struct mosquitto */*PORT_NAME*/_mosq = NULL;
 static int /*PORT_NAME*/_connected = 0;
-
-
-struct /*PORT_NAME*/_instance_type {
-    uint16_t listener_id;
-    /*INSTANCE_INFORMATION*/
-};
 
 extern struct /*PORT_NAME*/_instance_type /*PORT_NAME*/_instance;
 

compilers/official-network-plugins/src/main/resources/templates/PosixMQTTPlugin.h
@@ -1,7 +1,21 @@
 #ifndef /*PORT_NAME*/_PosixMQTT_Client_h
 #define  /*PORT_NAME*/_PosixMQTT_Client_h
 
-struct /*PORT_NAME*/_instance_type /*PORT_NAME*/_instance;
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <math.h>
+#include <mosquitto.h>
+/*INCLUDES*/
+
+
+struct /*PORT_NAME*/_instance_type {
+    uint16_t listener_id;
+    /*INSTANCE_INFORMATION*/
+} /*PORT_NAME*/_instance;
 
 void /*PORT_NAME*/_set_listener_id(uint16_t id);
 

compilers/official-network-plugins/src/main/resources/templates/PosixMQTTPluginX86.c
@@ -1,9 +1,4 @@
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#include <mosquitto.h>
+#include "/*PORT_NAME*/.h"
 
 #define /*PORT_NAME*/_ESCAPE_CHAR /*ESCAPE_CHAR*/
 
@@ -16,12 +11,6 @@ static int /*PORT_NAME*/_retain = 0;
 static uint16_t /*PORT_NAME*/_mid_sent = 0;
 struct mosquitto */*PORT_NAME*/_mosq = NULL;
 static int /*PORT_NAME*/_connected = 0;
-
-
-struct /*PORT_NAME*/_instance_type {
-    uint16_t listener_id;
-    /*INSTANCE_INFORMATION*/
-};
 
 extern struct /*PORT_NAME*/_instance_type /*PORT_NAME*/_instance;
 