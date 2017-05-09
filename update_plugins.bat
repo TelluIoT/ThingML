a909c7e92d97600f1231960e6849eb5374f3d829
brice-morin <brice.morin1@gmail.com>
2017-05-09 12:56:09 +0200

    cleanup

--- update_plugins.bat
+++
@@ -1,5 +0,0 @@
-call mvn clean install
-cd org.thingml.editor.standaloneApp
-call mvn -f pom_eclipse.xml clean install
-rm ../org.thingml.eclipse.ui/lib/*.*
-cp target/org.thingml.editor.standaloneApp-*-jar-with-dependencies.jar ../org.thingml.eclipse.ui/lib/thingml.jar
