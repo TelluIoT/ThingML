8afc24ce8389fa1bd8d65b7d81eedf502e246c81
Franck Fleurey <franck@fleurey.com>
2018-11-22 15:26:12 +0100

    progress on the java mqtt component

--- language/thingml.web/WebRoot/xtext-resources/generated/mode-thingml.js
+++ language/thingml.web/WebRoot/xtext-resources/generated/mode-thingml.js
@@ -1,3 +1,19 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ */
 define(["ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], function(oop, mText, mTextHighlightRules) {
 	var HighlightRules = function() {
 		var keywords = "abstract|action|and|as|composite|configuration|connector|datatype|do|else|end|entry|enumeration|error|errorln|event|exit|false|final|for|fork|fragment|from|function|guard|history|if|import|in|includes|init|instance|internal|keeps|message|not|object|on|optional|or|over|port|print|println|property|protocol|provided|readonly|receives|region|required|return|sends|session|set|state|statechart|thing|transition|true|var|while";
