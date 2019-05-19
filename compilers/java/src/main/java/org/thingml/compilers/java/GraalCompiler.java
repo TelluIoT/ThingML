a4948b3ec565d8bf5ae80ef7ee951f5b9b5dae92
brice-morin <brice.morin1@gmail.com>
2019-05-19 14:18:33 +0200

    ThingML can now wrap generated Java code into native apps, using GraalVM

--- compilers/java/src/main/java/org/thingml/compilers/java/GraalCompiler.java
+++ compilers/java/src/main/java/org/thingml/compilers/java/GraalCompiler.java
@@ -28,8 +28,6 @@ public class GraalCompiler extends JavaCompiler {
     public GraalCompiler() {
         super(new JavaThingActionCompiler(), new JavaThingApiCompiler(), new JavaCfgMainGenerator(),
                 new GraalCfgBuildCompiler(), new JavaThingImplCompiler());
-        connectorCompilers.clear();
-        connectorCompilers.put("swing", new Java2Swing());
     }
 
     @Override
@@ -52,17 +50,43 @@ public class GraalCompiler extends JavaCompiler {
     }
 
     @Override
+    public String getDockerBaseImage(Configuration cfg, Context ctx) {
+    	return "debian:stretch-slim";
+    }
+    
+    @Override
     public String getDockerCMD(Configuration cfg, Context ctx) {
-        return "java\", \"-jar\", \"" + cfg.getName() + "-1.0.0-jar-with-dependencies.jar";
+        return "/app";
     }
 
     @Override
     public String getDockerCfgRunPath(Configuration cfg, Context ctx) {
-        return "RUN cd /root && wget https://github.com/TelluIoT/ThingML/releases/download/1.0.0/mvn_repo_generated.tar.gz\n" +
-        		"RUN cd /root && tar -xzf mvn_repo_generated.tar.gz\n" +
-        		"COPY . .\n" +
-        		"RUN mvn install\n" +
-        		"FROM openjdk:jre-alpine\n" +
-        		"COPY --from=0 /target/" + cfg.getName() + "-1.0.0-jar-with-dependencies.jar .\n";
+        return "ENV JAVA_HOME /opt/graalvm\n" + 
+        		"ENV GRAALVM_HOME /opt/graalvm\n" + 
+        		"ENV NATIVE_IMAGE_CONFIG_FILE $GRAALVM_HOME/native-image.properties\n" + 
+        		"ENV PATH /opt/apache-maven/bin:$JAVA_HOME/jre/bin:$GRAALVM_HOME/bin:$PATH\n" + 
+        		"# All in one step, to reduce number of layers\n" + 
+        		"RUN apt-get update && " + 
+        		"    apt-get -y install gcc libc6-dev zlib1g-dev curl && " + 
+        		"    curl http://mirror.nbtelecom.com.br/apache/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz -o /tmp/maven.tar.gz && " + 
+        		"    tar -zxvf /tmp/maven.tar.gz -C /tmp && " + 
+        		"    mv /tmp/apache-maven-3.6.0 /opt/apache-maven && " + 
+        		"    curl -L https://github.com/oracle/graal/releases/download/vm-1.0.0-rc16/graalvm-ce-1.0.0-rc16-linux-amd64.tar.gz -o /tmp/graalvm.tar.gz && " + 
+        		"    tar -zxvf /tmp/graalvm.tar.gz -C /tmp && " + 
+        		"    mv /tmp/graalvm-ce-1.0.0-rc16 /opt/graalvm && " + 
+        		"    echo \"JavaArgs = -Xmn512k\" >> $NATIVE_IMAGE_CONFIG_FILE && " + 
+        		"    echo \"Args = -H:Kind=EXECUTABLE\" >> $NATIVE_IMAGE_CONFIG_FILE && " + 
+        		"    mkdir -p /root/.native-image && " + 
+        		"    rm -rf /var/lib/apt/lists/* && " + 
+        		"    rm -rf /tmp/*\n" + 
+        		"COPY . /root\n" + 
+        		"RUN cd /root && " + 
+        		"    curl -L https://github.com/TelluIoT/ThingML/releases/download/1.0.0/mvn_repo_generated.tar.gz -o ./mvn_repo_generated.tar.gz && " + 
+        		"    tar -xzf mvn_repo_generated.tar.gz && " + 
+        		"    rm mvn_repo_generated.tar.gz && " + 
+        		"    mvn install && mv /root/target/org.thingml.generated.main /app && rm -rf /root/target\n" + 
+        		"#FROM scratch\n" + 
+        		"FROM debian:stretch-slim\n" + 
+        		"COPY --from=0 app /app\n";
     }
 }
